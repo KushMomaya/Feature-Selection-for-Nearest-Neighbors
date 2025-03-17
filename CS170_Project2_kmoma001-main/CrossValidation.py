b07d90b81d7bb60e8d4e9092e6d50084203d80ee
KushMomaya <kushmomaya@gmail.com>
2025-03-16 21:23:42 -0700

    forward search should be fully functional using loocv stub currently

--- CS170_Project2_kmoma001-main/CrossValidation.py
+++ CS170_Project2_kmoma001-main/CrossValidation.py
@@ -1,3 +1,4 @@
+import numpy as np
 from NN import *
 
 data = np.loadtxt('../CS170_Small_Data__60.txt')
@@ -5,8 +6,5 @@ X = data[:, 1:]
 Y = data[:, 0]
 
 def loocv(X, Y, set, feature):
-    accuracy = 0
-    
-    
-
+    accuracy = np.random.random()
     return accuracy
--- CS170_Project2_kmoma001-main/FeatureSelection.py
+++ CS170_Project2_kmoma001-main/FeatureSelection.py
@@ -7,8 +7,6 @@ Y = data[:, 0]
 
 def forward_search(X, Y):
     current_set = []
-    best_set = []
-    best_accuracy = 0
 
     for i in range(1, len(X[0]) + 1):
         print("On the " + str(i) + "th level of the search tree")
@@ -19,19 +17,16 @@ def forward_search(X, Y):
             if k in current_set:
                 continue
 
-            print("Considering adding the " + str(k) + "th feature")
+            print("Considering adding feature " + str(k))
             accuracy = loocv(X, Y, current_set, k)
 
-            if accuracy > best_accuracy:
+            if accuracy > best_sofar_accuracy:
                 best_sofar_accuracy = accuracy
                 feature_to_add = k
 
         current_set.append(feature_to_add)
-        print("Added feature " + str(feature_to_add) + " to current set")
-
-        if best_sofar_accuracy > best_accuracy:
-            best_accuracy = best_sofar_accuracy
-            best_set = current_set
+        print("On level " + str(i) + " I added feature " + str(feature_to_add) + " to the current set")
+        print("Accuracy is " + str(best_sofar_accuracy))
 
 
 forward_search(X, Y)
