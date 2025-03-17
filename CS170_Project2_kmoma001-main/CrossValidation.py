bba870370e0a562834f0aa1580f30c9ff7fe9040
KushMomaya <kushmomaya@gmail.com>
2025-03-16 21:43:04 -0700

    removed NN file, nn logic inside CrossValidation, now need to configure loocv to accept feature set, and feature for forward and backwards search

--- CS170_Project2_kmoma001-main/CrossValidation.py
+++ CS170_Project2_kmoma001-main/CrossValidation.py
@@ -5,6 +5,29 @@ data = np.loadtxt('../CS170_Small_Data__60.txt')
 X = data[:, 1:]
 Y = data[:, 0]
 
-def loocv(X, Y, set, feature):
-    accuracy = np.random.random()
+def loocv(X, Y): #, set, feature):
+    num_correct = 0
+    accuracy = 0
+    for i in range(len(X)):
+        point = X[i]
+        label = Y[i]
+        nn_distance = float('inf')
+        nn_location = None
+        nn_label = None
+        for k in range(len(X)):
+            if k == i:
+                continue
+            distance = np.sqrt(np.sum((point - X[k]) ** 2))
+            if distance < nn_distance:
+                nn_distance = distance
+                nn_location = k
+                nn_label = Y[nn_location]
+        #print("Point " + str(i+1) + " is class " + str(label) + " and its nearest neighbor is point " + str(nn_location+1) + " which is class " + str(nn_label))
+        if label == nn_label:
+            num_correct += 1
+
+    accuracy = num_correct / len(X)
     return accuracy
+
+
+print(loocv(X, Y))
