621011c3e714457e192fcc6fb15d698fe9d53b80
KushMomaya <kushmomaya@gmail.com>
2025-03-17 19:08:22 -0700

    done with backwards select, need to add main

--- CS170_Project2_kmoma001-main/FeatureSelection.py
+++ CS170_Project2_kmoma001-main/FeatureSelection.py
@@ -1,16 +1,16 @@
 import numpy as np
 from CrossValidation import *
 
-data = np.loadtxt('../CS170_Small_Data__60.txt')
-X = data[:, 1:]
-Y = data[:, 0]
+#data = np.loadtxt('../CS170_Small_Data__60.txt')
+#X = data[:, 1:]
+#Y = data[:, 0]
 
 def forward_search(X, Y):
     current_set = []
 
     for i in range(len(X[0])):
         print("On the " + str(i+1) + "th level of the search tree")
-        feature_to_add = None
+        feature_to_add = 0
         best_sofar_accuracy = 0
 
         for k in range(len(X[0])):
@@ -25,8 +25,41 @@ def forward_search(X, Y):
                 feature_to_add = k
 
         current_set.append(feature_to_add)
+        print("")
         print("On level " + str(i+1) + " I added feature " + str(feature_to_add+1) + " to the current set")
+        print("")
         print("Accuracy is " + str(best_sofar_accuracy))
+        print("")
+        print("Current Set is " + str([j + 1 for j in current_set]))
+        print("")
 
 
-forward_search(X, Y)
+
+
+def backward_search(X, Y):
+    current_set = np.array([i for i in range(len(X[0]))])
+
+    for i in range(len(X[0])):
+        print("On the " + str(i+1) + "th level of the search tree")
+        feature_to_remove = None
+        best_sofar_accuracy = 0
+
+        for k in range(len(current_set)):
+            print("Considering removing feature " + str(current_set[k]+1))
+            accuracy = loocv(X, Y, np.delete(current_set, k), 0)
+
+            if accuracy > best_sofar_accuracy:
+                best_sofar_accuracy = accuracy
+                feature_to_remove = k
+        removed_feature = current_set[feature_to_remove]
+        current_set = np.delete(current_set, feature_to_remove)
+        print("")
+        print("On level " + str(i+1) + " I removed feature " + str(removed_feature+1) + " from the current set")
+        print("")
+        print("Accuracy is " + str(best_sofar_accuracy))
+        print("")
+        print("Current Set is " + str([int(j + 1) for j in current_set]))
+        print("")
+
+#forward_search(X, Y)
+#backward_search(X, Y)
